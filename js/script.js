3aad7f2be146e21517f58db09585228cd58bc250
Philippe <pguyard@gmail.com>
2016-10-12 18:15:10 +0100

    Cleaned up files and added Google Map

--- js/script.js
+++ js/script.js
@@ -94,6 +94,46 @@ mwcApp.run(function($rootScope, $location, $anchorScroll, $routeParams) {
     $scope.pageClass = 'page-workwithus';
   });
 
+// Google Map Controller /////////////////////////////////////////////////////
+mwcApp.controller('MapCtrl', function($scope) {
+
+var mapOptions = {
+  zoom: 4,
+  center: new google.maps.LatLng(40.0000, -98.0000),
+  mapTypeId: google.maps.MapTypeId.TERRAIN
+}
+
+$scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+$scope.markers = [];
+
+var infoWindow = new google.maps.InfoWindow();
+
+var createMarker = function(info) {
+
+  var marker = new google.maps.Marker({
+    map: $scope.map,
+    position: new google.maps.LatLng(info.lat, info.long),
+    title: info.city
+  });
+  marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
+    infoWindow.open($scope.map, marker);
+  });
+
+  $scope.markers.push(marker);
+
+}
+
+$scope.openInfoWindow = function(e, selectedMarker) {
+  e.preventDefault();
+  google.maps.event.trigger(selectedMarker, 'click');
+}
+
+});
+//end Google Map Controller////////////////////////////////////////////////
   function DialogController($scope, $mdDialog) {
     $scope.hide = function() {
       $mdDialog.hide();
