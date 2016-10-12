2b2a4138d3db5e3ccec420fd9c6aa9c1166ba5bb
Philippe <pguyard@gmail.com>
2016-10-12 16:24:32 +0100

    Added modals to home page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
   // create the module and name it mwcApp
-  var mwcApp = angular.module('mwcApp', ['ngRoute', 'ngAnimate']);
+  var mwcApp = angular.module('mwcApp', ['ngRoute', 'ngAnimate','ngMaterial', 'ngMessages']);
 
   // configure our routes
   mwcApp.config(function($routeProvider) {
@@ -36,7 +36,7 @@
       });
   });
 
-// this is needed for bookmark links to work
+// this is needed for deep-linking (# bookmark links) to work
 // http://stackoverflow.com/questions/31620412/scrolling-to-specific-element-on-different-template-after-clicking-link-angular
 mwcApp.run(function($rootScope, $location, $anchorScroll, $routeParams) {
   $rootScope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
@@ -46,10 +46,37 @@ mwcApp.run(function($rootScope, $location, $anchorScroll, $routeParams) {
 })
 
     // create the controller and inject Angular's $scope
-  mwcApp.controller('mainController', function($scope,$location, $anchorScroll, $routeParams) {
+  mwcApp.controller('mainController', function($scope,$location, $anchorScroll, $routeParams,$mdDialog) {
     // create a message to display in our view
     $scope.message = 'this is the home page!';
     $scope.pageClass = 'page-home'; //sets individual class for each page (not used at the moment)
+    
+    $scope.status = '  ';
+    $scope.customFullscreen = false;
+
+    $scope.showTradMthd = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'templates/tradmthd.tmpl.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+      })    
+    };
+    $scope.showReachWash = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: 'templates/reachwash.tmpl.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true,
+      fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.      
+      })        
+    };
+
+
+
   });
 
   mwcApp.controller('pricesController', function($scope,$location, $anchorScroll, $routeParams) {
@@ -66,3 +93,18 @@ mwcApp.run(function($rootScope, $location, $anchorScroll, $routeParams) {
     $scope.message = 'Work With My WindowCleaner';
     $scope.pageClass = 'page-workwithus';
   });
+
+  function DialogController($scope, $mdDialog) {
+    $scope.hide = function() {
+      $mdDialog.hide();
+    };
+
+    $scope.cancel = function() {
+      $mdDialog.cancel();
+    };
+
+    $scope.answer = function(answer) {
+      $mdDialog.hide(answer);
+    };
+  }
+
