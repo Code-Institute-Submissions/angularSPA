2c8c18ec8ac521a8a9e070d4415a4cb50e277e00
Philippe <pguyard@gmail.com>
2016-10-13 09:25:28 +0100

    Tidy up

--- js/bignavbar.js
+++ js/bignavbar.js
@@ -1,23 +1,17 @@
-var navbarHeight = $('.navbar').height(); 
-
+var navbarHeight = $('.navbar').height();
   $(window).scroll(function() {
   var navbarColor = "62,195,246";//color attr for rgba
   var smallLogoHeight = $('.small-logo').height();
-  var bigLogoHeight = $('.big-logo').height();
-  
+  var bigLogoHeight = $('.big-logo').height();  
   
   var smallLogoEndPos = 0;
-  var smallSpeed = (smallLogoHeight / bigLogoHeight);
-  
-  var ySmall = ($(window).scrollTop() * smallSpeed); 
-  
+  var smallSpeed = (smallLogoHeight / bigLogoHeight);  
+  var ySmall = ($(window).scrollTop() * smallSpeed);   
   var smallPadding = navbarHeight - ySmall;
   if (smallPadding > navbarHeight) { smallPadding = navbarHeight; }
   if (smallPadding < smallLogoEndPos) { smallPadding = smallLogoEndPos; }
-  if (smallPadding < 0) { smallPadding = 0; }
-  
-  $('.small-logo-container ').css({ "padding-top": smallPadding});
-  
+  if (smallPadding < 0) { smallPadding = 0; }  
+  $('.small-logo-container ').css({ "padding-top": smallPadding});  
   var navOpacity = ySmall / smallLogoHeight; 
   if  (navOpacity > 1) { navOpacity = 1; }
   if (navOpacity < 0 ) { navOpacity = 0; }
@@ -29,6 +23,5 @@ var navbarHeight = $('.navbar').height();
     $('.navbar').css({"box-shadow": "0 2px 3px rgba(0,0,0," + shadowOpacity + ")"});
   } else {
     $('.navbar').css({"box-shadow": "none"});
-     }
-  
+     }  
 });
